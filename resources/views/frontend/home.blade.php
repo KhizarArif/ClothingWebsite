ac2f1542fe732adef40499b245115130bf5f1764
KhizarArif <khizararif201@gmail.com>
2024-08-06 07:24:21 +0500

    Product Images Styling

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,12 +5,12 @@
 <section id="product-cards">
 
     <div class="container-fluid">
-        <div class="row row-cols-2 row-cols-md-4 g-4" id="product-list">
+        <div class="row row-cols-2 row-cols-md-3 g-4" id="product-list">
             @if ($products != null)
             @foreach ($products as $product )
 
-            <div class="col filter-item all new">
-                <div class="card h-100">
+            <div class="col-md-3 col-sm-6 col-xs-12 filter-item all new">
+                <div class="card">
                     <div class="img-container position-relative">
                         @if ($product->product_images != null)
                         <img src="{{asset('frontend_assets/image/products/'.$product->product_images->first()->image)}}" class="card-img-top shop-item-image" alt="{{$product->title}}">
@@ -26,9 +26,9 @@
                     </div>
                     <div class="card-body d-flex flex-column">
                         <h5 class="card-title shop-item-title">{{$product->title}}</h5>
-                        <p class="card-text shop-item-price">
-                            <span class="discounted-price">Rs. {{$product->price}}</span> <br>
-                            <span class="original-price">Rs. {{$product->original_price}}</span>
+                        <p class="card-text shop-item-price w-100 d-flex justify-content-between">
+                            <span class="original-price">Rs. {{$product->original_price}} PKR</span>
+                            <span class="discounted-price">Rs. {{$product->price}} PKR</span> 
                         </p>
                     </div>
                 </div>
